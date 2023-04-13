2738d256e6c310b146fd78942680dec08b4e912d
Lee Si Woo <asonaso@kpu.ac.kr>
2023-04-13 17:35:28 +0900

    MainScene 추가

---
+++ RhythmHero/app/src/main/java/kr/ac/tukorea/ge/rhythmhero/a2020182033/app/MainActivity.java
@@ -0,0 +1,42 @@
+package kr.ac.tukorea.ge.rhythmhero.a2020182033.app;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import kr.ac.tukorea.ge.rhythmhero.a2020182033.framework.scene.BaseScene;
+import kr.ac.tukorea.ge.rhythmhero.a2020182033.framework.view.GameView;
+import kr.ac.tukorea.ge.rhythmhero.a2020182033.game.MainScene;
+
+public class MainActivity extends AppCompatActivity {
+
+    private GameView gameView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        gameView = new GameView(this);
+        gameView.setFullScreen();
+        setContentView(gameView);
+
+        new MainScene().pushScene();
+    }
+
+    @Override
+    protected void onPause() {
+        gameView.pauseGame();
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        gameView.resumeGame();
+    }
+
+    @Override
+    protected void onDestroy() {
+        BaseScene.popAll();
+        super.onDestroy();
+    }
+}
---
+++ RhythmHero/app/src/main/java/kr/ac/tukorea/ge/rhythmhero/a2020182033/game/MainScene.java
@@ -0,0 +1,10 @@
+package kr.ac.tukorea.ge.rhythmhero.a2020182033.game;
+
+
+import kr.ac.tukorea.ge.rhythmhero.a2020182033.framework.scene.BaseScene;
+
+public class MainScene extends BaseScene {
+    private static final String TAG = MainScene.class.getSimpleName();
+
+}
+
