8344daaa90fd76267b04254cc91085870869890f
Lee Si Woo <asonaso@kpu.ac.kr>
2023-04-14 00:34:33 +0900

    마크 클래스 추가

---
+++ RhythmHero/app/src/main/java/kr/ac/tukorea/ge/rhythmhero/a2020182033/game/Mark.java
@@ -0,0 +1,25 @@
+package kr.ac.tukorea.ge.rhythmhero.a2020182033.game;
+
+import android.graphics.Canvas;
+import android.graphics.RectF;
+
+import kr.ac.tukorea.ge.rhythmhero.a2020182033.framework.interfaces.IGameObject;
+
+public class Mark implements IGameObject {
+
+    protected RectF collisionRect = new RectF();
+    protected static float appeared_timing;
+
+    protected Mark(float appeared_timing) {
+        super();
+        this.appeared_timing = appeared_timing;
+    }
+
+    @Override
+    public void update() {
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+    }
+}
