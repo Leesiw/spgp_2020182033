734560eb8470d1193b21b2a65b2c88d022a3ba06
Lee Si Woo <asonaso@kpu.ac.kr>
2023-06-10 16:47:23 +0900

    필요없는것 제거

--- RhythmHero/app/src/main/java/kr/ac/tukorea/ge/rhythmhero/a2020182033/game/Scene/PausedScene.java
+++ RhythmHero/app/src/main/java/kr/ac/tukorea/ge/rhythmhero/a2020182033/game/Scene/PausedScene.java
@@ -15,10 +15,8 @@ public class PausedScene extends BaseScene {
     }
     public PausedScene() {
         initLayers(Layer.COUNT);
-        //add(Layer.bg, new Sprite(R.mipmap.bg_city_landscape, 8.0f, 4.5f, 16, 9));
         add(Layer.bg, new Sprite(R.mipmap.bg_menu, 8.0f, 4.5f, 10, 5.75f));
-        //title = new Sprite(R.mipmap.cookie_run_title, 8.0f, 4.5f, 3.69f, 1.36f);
-        //add(Layer.bg, title);
+
         add(Layer.touch, new Button(R.mipmap.btn_resume, 8f, 3.5f, 4.667f, 3f, new Button.Callback() {
             @Override
             public boolean onTouch(Button.Action action) {
@@ -40,15 +38,7 @@ public class PausedScene extends BaseScene {
             }
         }));
     }
-
-    @Override
-    public void update(long elapsedNanos) {
-        super.update(elapsedNanos);
-        float x = (float) (8.0f + 4.0f * Math.cos(angle));
-        float y = (float) (4.5f + 2.0f * Math.sin(angle));
-       // title.moveTo(x, y);
-    }
-
+    
     @Override
     public boolean isTransparent() {
         return true;
